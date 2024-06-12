cbd48fe7dea0deb377abc5d2d2f03206eda6d0f5
suupoo <git@suu-poo.com>
2024-06-13 00:46:14 +0900

    add:ソート用リンク埋め込み

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts')
 
 @section('content')
+@php
+    $currentRouteName = request()->route()->getName();
+    $sort = request()->get('sort');
+    $direction = request()->get('direction');
+@endphp
 <div class="flex flex-col gap-2 px-12 py-2">
     <h1 class="text-xl font-bold">
         {{ $model::NAME }}
@@ -10,7 +15,31 @@
         <thead>
             <tr>
                 @foreach($model::getColumns() as $column)
-                <th class="border border-slate-300">{{ $column->label() }}</th>
+                <th class="border border-slate-300 p-2">
+                    <div class="flex w-full items-center justify-center space-x-1">
+                        <a
+                            class="
+                                bg-gray-100
+                                p-0.5
+                                @if($sort === $column->column() && $direction == 'asc' ) text-gray-800 @else text-gray-400 @endif
+                            "
+                            href="{{ route($currentRouteName, ['sort' => $column->column(), 'direction' => 'asc'])}}"
+                        >
+                            @include('components.list.sort-up')
+                        </a>
+                        <span>{{ $column->label() }}</span>
+                        <a
+                            class="
+                                bg-gray-100
+                                p-0.5
+                                @if($sort === $column->column() && $direction == 'desc' ) text-gray-800 @else text-gray-400 @endif
+                            "
+                            href="{{ route($currentRouteName, ['sort' => $column->column(), 'direction' => 'desc'])}}"
+                        >
+                            @include('components.list.sort-down')
+                        </a>
+                    </div>
+                </th>
                 @endforeach
             </tr>
         </thead>
