05af932626f206a92067fa8211197e06d2293af7
suupoo <git@suu-poo.com>
2024-06-21 01:06:02 +0900

    ✨:メール送信クラスのサンプルを追加

---
+++ resources/lang/ja/mail.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'user' => [
+        'verify_email' => [
+            'system' => [
+                'subject' => 'メールアドレス変更の確認',
+            ],
+        ],
+    ],
+];
