a2a64018232998219f4ad010ffe6089f84c9957b
suupoo <git@suu-poo.com>
2024-06-21 01:26:23 +0900

    ✨:ユーザ新規登録およびメールアドレス編集後に認証メールを飛ばす

--- app/UseCases/UserAction/CreateAction.php
+++ app/UseCases/UserAction/CreateAction.php
@@ -2,7 +2,11 @@
 
 namespace App\UseCases\UserAction;
 
+use App\Mail\User\VerifyEmailFromSystem;
 use App\UseCases\ResourceAction\CreateAction as BaseAction;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 /**
  * Class CreateAction
@@ -10,4 +14,23 @@ use App\UseCases\ResourceAction\CreateAction as BaseAction;
  */
 class CreateAction extends BaseAction
 {
+    /**
+     * コミット後の処理
+     * @param Request $request
+     * @param string $model
+     * @param array $attributes
+     * @return void
+     */
+    public function afterOfCommit(Request $request, string $model, array $attributes = []): void
+    {
+        try {
+            $entity = $attributes['entity'];
+            // 認証メールを送信する
+            $email = $entity->email;
+            Mail::to($email)->send(new VerifyEmailFromSystem());
+
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+        }
+    }
 }
--- app/UseCases/UserAction/UpdateAction.php
+++ app/UseCases/UserAction/UpdateAction.php
@@ -2,9 +2,11 @@
 
 namespace App\UseCases\UserAction;
 
+use App\Mail\User\VerifyEmailFromSystem;
 use App\UseCases\ResourceAction\UpdateAction as BaseAction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 /**
  * Class CreateAction
@@ -12,9 +14,25 @@ use Illuminate\Support\Facades\Log;
  */
 class UpdateAction extends BaseAction
 {
-    public function afterOfUpdate(Request $request, string $model, array $attributes = []): void
+    /**
+     * コミット後の処理
+     * @param Request $request
+     * @param string $model
+     * @param array $attributes
+     * @return void
+     */
+    public function afterOfCommit(Request $request, string $model, array $attributes = []): void
     {
-        // todo:ここで認証用のメールを送る
-        Log::info('send mail to user.');
+        try {
+            $entity = $attributes['entity'];
+            // メールアドレスが変更されている場合は認証メールを送信する
+            if( $entity->wasChanged('email') ){
+                $email = $entity->email;
+                Mail::to($email)->send(new VerifyEmailFromSystem());
+            }
+
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+        }
     }
 }
