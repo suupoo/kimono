c3293aaa7609b9739ec2203ab12e713ddd8cb246
suupoo <git@suu-poo.com>
2024-06-26 01:43:29 +0900

    🎨:SP版の場合は編集などのボタンはアイコンを表示する

--- resources/views/components/button/edit-link.blade.php
+++ resources/views/components/button/edit-link.blade.php
@@ -1,5 +1,6 @@
 <a
     href="{{ $link }}"
     class="w-full text-blue-500 text-center border border-blue-500 font-bold py-2 px-4 rounded">
-    {{ __('resource.edit') }}
+    <span class="hidden md:inline">{{ __('resource.edit') }}</span>
+    <span class="inline md:hidden">@include('components.button.icons.edit')</span>
 </a>
--- resources/views/components/button/show-link.blade.php
+++ resources/views/components/button/show-link.blade.php
@@ -1,5 +1,6 @@
 <a
     href="{{ $link }}"
     class="text-gray-500 text-center border border-gray-500 font-bold py-2 px-4 rounded">
-    {{ __('resource.show') }}
+    <span class="hidden md:inline">{{ __('resource.show') }}</span>
+    <span class="inline md:hidden">@include('components.button.icons.detail')</span>
 </a>
