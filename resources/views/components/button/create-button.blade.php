2c5c82defaee613e3402337d2c0a48538427e185
suupoo <git@suu-poo.com>
2024-06-26 02:14:25 +0900

    🎨:デザイン調整

--- resources/views/components/button/create-button.blade.php
+++ resources/views/components/button/create-button.blade.php
@@ -1,5 +1,5 @@
 <input
     type="submit"
-    class="w-full bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+    class="w-full hover:text-white  bg-green-500 hover:bg-green-500 text-white font-bold py-2 px-4 rounded"
     value="{{ __('resource.store') }}"
 />
