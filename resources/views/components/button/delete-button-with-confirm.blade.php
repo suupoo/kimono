a3c14b168791115db9b401ad1a4e8e3e6131e187
suupoo <git@suu-poo.com>
2024-06-26 14:42:30 +0900

    🎨:削除ボタンのテキスト誤りを修正

--- resources/views/components/button/delete-button-with-confirm.blade.php
+++ resources/views/components/button/delete-button-with-confirm.blade.php
@@ -1,6 +1,6 @@
 <button data-modal-target="popup-modal" data-modal-toggle="popup-modal"
         class="bg-red-500 text-white hover:text-red-500 hover:bg-white text-center border border-red-500 font-bold py-2 px-4 rounded">
-    <span class="hidden md:inline">{{ __('resource.edit') }}</span>
+    <span class="hidden md:inline">{{ __('resource.delete') }}</span>
     <span class="inline md:hidden">@include('components.button.icons.trash-bin')</span>
 </button>
 
