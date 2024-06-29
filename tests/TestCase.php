a9c7424fdec59221e7c75fba9fbd9efef168642e
suupoo <git@suu-poo.com>
2024-06-29 11:18:10 +0900

    🚨:テスト時のログインを共通化

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,10 +2,20 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 abstract class TestCase extends BaseTestCase
 {
     use RefreshDatabase;
+
+    protected function login($user = null)
+    {
+        $user = $user ?? User::factory()->create();
+
+        $this->actingAs($user);
+
+        return $user;
+    }
 }
