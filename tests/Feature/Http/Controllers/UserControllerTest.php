d457d0bde412024a2dfb7aeeeb0ede3c11a61425
suupoo <git@suu-poo.com>
2024-06-29 11:45:22 +0900

    🚨:保存時のテスト

--- tests/Feature/Http/Controllers/UserControllerTest.php
+++ tests/Feature/Http/Controllers/UserControllerTest.php
@@ -2,13 +2,14 @@
 
 namespace Tests\Feature\Http\Controllers;
 
-use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\User as ResourceModel; // モデル紐付け
+use App\Enums\UserRole;
 use Tests\TestCase;
 
 class UserControllerTest extends TestCase
 {
+    protected string $resourcePrefix = 'users';
+
     public function test_非ログイン時はユーザ一覧からログインページへリダイレクト(): void
     {
         $response = $this->get('/');
@@ -22,11 +23,33 @@ class UserControllerTest extends TestCase
         $this->login();
 
         // Act（実行）
-        $response = $this->get('/users?sort=id&order=asc');
+        $response = $this->get("/$this->resourcePrefix}?sort=id&order=asc'");
 
         // Assert（検証）
         $response
             ->assertOk()
             ->assertSee(parent::LOGIN_USER_NAME);
+    }
+
+    public function test_ログイン時にユーザの新規登録ができる(): void
+    {
+        // ログイン
+        $this->login();
+
+        $storeData = [
+            'name' => 'テスト太郎',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'role' => UserRole::NORMAL,
+        ];
+
+        // 登録処理
+        $response = $this->post(route("{$this->resourcePrefix}.store"), $storeData);
+
+        // 登録後のリダイレクト先が正しいか
+        $response->assertRedirect(route("{$this->resourcePrefix}.edit", ResourceModel::first()));
+
+        // 登録データがDBに保存されているか
+        $this->assertDatabaseHas('users', $storeData);
     }
 }
