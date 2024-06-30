676a54b70b0e47d6980463838d48e270f74bf460
suupoo <git@suu-poo.com>
2024-07-01 01:46:52 +0900

    🐛:顧客の新規登録時、顧客名がなくなっていたのを修正

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -17,7 +17,7 @@
             @foreach($model::getColumns() as $column)
                 <div class="w-full my-1">
 
-                    @if($column instanceof \App\ValueObjects\Customer\Name)
+                    @if($column instanceof \App\ValueObjects\Customer\CustomerName)
                         {!! $column->input(['required' => true, 'class' => ''])?->render() !!}
                     @endif
 
