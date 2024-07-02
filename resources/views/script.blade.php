2eb818512af371e62e794adc76a77dee0ed1710e
suupoo <git@suu-poo.com>
2024-07-03 00:04:33 +0900

    🎨:郵便番号入力欄0000000はフォーカスアウトで-を自動で入れる

--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -2,8 +2,10 @@
     $(function(){
         $('input[name=post_code]').on('focusout', function(){
             let postCode = $(this).val();
-                postCode = postCode.slice(0, 3) + '-' + postCode.slice(3);
-                $(this).val(postCode);
+            if(postCode.match(/^\d{7}$/)){
+                $(this).val(postCode.substr(0, 3) + '-' + postCode.substr(3, 4));
+            }
+
         });
     })
 </script>
