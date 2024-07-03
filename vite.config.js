810cbf276a4c0aa83a3ccabf7a602cad8581c437
suupoo <git@suu-poo.com>
2024-07-03 22:59:08 +0900

    🐛:ローカルでVITEのホットリロードが動作しなかったためポート変更

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
     ],
     server: {
         host: true,
+        port: 8078,
         https: {
             key: fs.readFileSync('/etc/ssh/localhost-key.pem'),
             cert: fs.readFileSync('/etc/ssh/localhost.pem'),
