8c9a9ae33e7babd0899349931c3af381744f54af
suupoo <git@suu-poo.com>
2024-07-05 02:09:16 +0900

    ♻:ログインボタンのコンポーネントを共通化

--- resources/views/components/auth/login-submit.blade.php
+++ resources/views/components/auth/login-submit.blade.php
@@ -1,5 +1,4 @@
-<input
-    type="submit"
-    class="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded"
-    value="{{ __('Login') }}"
-/>
+<x-button.button {{ $attributes->merge(['type' => 'submit', 'class' => 'bg-custom-blue text-white hover:bg-white hover:text-custom-blue']) }}>
+    {{ __('Login') }}
+</x-button.button>
+
