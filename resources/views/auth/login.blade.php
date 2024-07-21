bbb6869e2cbf9a1e358463d30bfc6c5a2a93b210
suupoo <git@suu-poo.com>
2024-07-22 02:34:08 +0900

    🎨デザイン調整

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,16 +19,16 @@
             <h1 class="text-xl text-center font-bold mt-3">
                 {{ __('Login') }}
             </h1>
-            <div class="flex flex-col w-full">
+            <div class="flex flex-col w-full gap-2">
                 @foreach($model::getColumns() as $column)
                     <div class="w-full my-1">
 
                         @if($column instanceof \App\ValueObjects\Administrator\Email)
-                            {!! $column->input(['required' => true, 'required_hidden' => true, 'class' => ''])?->render() !!}
+                            {!! $column->input(['required' => true, 'required_hidden' => true, 'class' => ''])!!}
                         @endif
 
                         @if($column instanceof \App\ValueObjects\Administrator\Password)
-                            {!! $column->input(['required' => true, 'required_hidden' => true, 'class' => ''])?->render() !!}
+                            {!! $column->input(['required' => true, 'required_hidden' => true, 'class' => '']) !!}
                         @endif
 
                     </div>
