1004ef7c60cae61decd6dbdc28c3d158011b2627
suupoo <git@suu-poo.com>
2024-07-25 00:23:05 +0900

    🎨:ログイン画面のデザイン修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,16 +19,16 @@
             <h1 class="text-xl text-center font-bold mt-3">
                 {{ __('Login') }}
             </h1>
-            <div class="flex flex-col w-full gap-2">
+            <div class="flex flex-col w-full">
                 @foreach($model::getColumns() as $column)
                     <div class="w-full my-1">
 
                         @if($column instanceof \App\ValueObjects\Administrator\Email)
-                            {!! $column->input(['required' => true, 'required_hidden' => true, 'class' => ''])!!}
+                            {!! $column->input(['required' => true, 'required_hidden' => true, 'class' => '!h-8'])!!}
                         @endif
 
                         @if($column instanceof \App\ValueObjects\Administrator\Password)
-                            {!! $column->input(['required' => true, 'required_hidden' => true, 'class' => '']) !!}
+                            {!! $column->input(['required' => true, 'required_hidden' => true, 'class' => '!h-8']) !!}
                         @endif
 
                     </div>
