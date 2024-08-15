7b86ea2643badc707e55bf693c0f609b278d0193
suupoo <git@suu-poo.com>
2024-08-15 22:08:20 +0900

    🎨:ヘッダー・サイドメニューの実装

---
+++ resources/views/components/header/header-item.blade.php
@@ -0,0 +1,6 @@
+<a {{ $attributes->merge([
+    'class' => 'p-2 md:px-3 flex items-center text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:hover:text-neutral-300 dark:focus:bg-neutral-700 dark:focus:text-neutral-300',
+]) }}
+>
+    {{ $slot }}
+</a>
--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -11,38 +11,61 @@
         <link rel="shortcut icon" href="{{ Vite::asset('resources/images/favicon.ico') }}">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-noto-jp antialiased bg-gray-50 text-black">
-        <div class="min-h-screen">
-            <x-menu.topMenu/>
-            <x-menu.sideMenu/>
-            <div class="p-4 sm:ml-64 min-h-screen mb-24">
-                <div class="p-1 md:px-4 mt-14">
-                    <div class="flex flex-col h-full mb-4 rounded">
-                        @breadcrumbs
-                        {{-- ぱんくずリストを使用する/しない(デフォルト:True） --}}
-                        {{-- .env CUSTOM_BREADCRUMBS_USEで設定可能とする --}}
-                        <div class="my-2">
-                           {{ Breadcrumbs::render(\Illuminate\Support\Facades\Route::currentRouteName()) }}
-                        </div>
-                        @endbreadcrumbs
+    <body class="font-noto-jp antialiased text-black">
+        {{-- header-nav --}}
+        <header class="flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full bg-white border-b border-gray-200 dark:bg-neutral-800 dark:border-neutral-700">
+            <x-header.header />
+        </header>
+        {{-- side-nav --}}
+        <x-menu.sideMenu />
 
-                        @yield('content')
-                    </div>
+{{--        <div class="min-h-screen">--}}
+{{--            <x-menu.topMenu/>--}}
+{{--            <x-menu.sideMenu/>--}}
+{{--            <div class="p-4 sm:ml-64 min-h-screen mb-24">--}}
+{{--                <div class="p-1 md:px-4 mt-14">--}}
+{{--                    <div class="flex flex-col h-full mb-4 rounded">--}}
+{{--                        @breadcrumbs--}}
+{{--                        --}}{{-- ぱんくずリストを使用する/しない(デフォルト:True） --}}
+{{--                        --}}{{-- .env CUSTOM_BREADCRUMBS_USEで設定可能とする --}}
+{{--                        <div class="my-2">--}}
+{{--                           {{ Breadcrumbs::render(\Illuminate\Support\Facades\Route::currentRouteName()) }}--}}
+{{--                        </div>--}}
+{{--                        @endbreadcrumbs--}}
+
+{{--                        @yield('content')--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--        <div class="fixed right-0 z-50 w-full min-h-20 bottom-0 bg-gray-400 bg-opacity-20" data-ad="banner">--}}
+{{--            <div class="relative">--}}
+{{--                <button class="absolute right-0 ad-close">--}}
+{{--                    @includeIf('icons.close', ['class' => 'w-6 h-6 p-1'])--}}
+{{--                </button>--}}
+{{--                <div class="flex flex-wrap justify-between">--}}
+{{--                    <img src="" class="object-fill w-full md:w-1/2 h-[60px]" alt="広告バナー1">--}}
+{{--                    <img src="" class="object-fill w-full md:w-1/2 h-[60px]" alt="広告バナー2">--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--        @include('script')--}}
+{{--        @yield('page-script')--}}
+
+        <main id="content">
+            <div class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="py-10 min-h-[80vh] bg-white dark:bg-neutral-800 ">
+                    @yield('content')
                 </div>
             </div>
-        </div>
-        <div class="fixed right-0 z-50 w-full min-h-20 bottom-0 bg-gray-400 bg-opacity-20" data-ad="banner">
-            <div class="relative">
-                <button class="absolute right-0 ad-close">
-                    @includeIf('icons.close', ['class' => 'w-6 h-6 p-1'])
-                </button>
-                <div class="flex flex-wrap justify-between">
-                    <img src="" class="object-fill w-full md:w-1/2 h-[60px]" alt="広告バナー1">
-                    <img src="" class="object-fill w-full md:w-1/2 h-[60px]" alt="広告バナー2">
-                </div>
-            </div>
-        </div>
-        @include('script')
+        </main>
+
+        <footer class="w-full min-h-[10vh] mx-auto px-4 sm:px-6 lg:px-8">
+            <x-footer.footer />
+        </footer>
+        <script type="module" src="{{ asset('js/script.js') }}">
+            @include('script')
+        </script>
         @yield('page-script')
     </body>
 </html>
