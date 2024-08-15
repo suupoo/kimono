0c432faf737e107d8ebeb424e1138669f03ebc16
suupoo <git@suu-poo.com>
2024-08-16 01:20:41 +0900

    🎨: inputでファイル選択時のクラス指定を追加

--- resources/views/components/custom-form/input.blade.php
+++ resources/views/components/custom-form/input.blade.php
@@ -15,6 +15,9 @@
     $w = ($type == 'checkbox') ? 'w-5' : 'w-full';
     $h = ($type == 'checkbox') ? 'h-5' : 'h-full';
     $fileAccept = $attributes['fileAccept'] ?? '';
+    if ($type == 'file') {
+        $additionalClass = $additionalClass." file:bg-gray-50 file:border-0 file:me-4 file:py-3 file:px-4";
+    }
 @endphp
 <input
     class="{{ $w }} {{ $h }} py-2 px-3 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600 {{ $additionalClass }}"
