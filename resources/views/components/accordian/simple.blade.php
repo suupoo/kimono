c19e0ec64a72d33d75ab86c819ad66b1fecdccc1
suupoo <git@suu-poo.com>
2024-08-15 23:50:49 +0900

    🎨:検索エリアをpreline移管

---
+++ resources/views/components/accordian/simple.blade.php
@@ -0,0 +1,19 @@
+<div class="hs-accordion-group">
+    <div class="hs-accordion active" id="hs-basic-heading-one">
+        <button class="hs-accordion-toggle hs-accordion-active:text-blue-600 py-3 inline-flex items-center gap-x-3 w-full font-semibold text-start text-gray-800 hover:text-gray-500 focus:outline-none focus:text-gray-500 rounded-lg disabled:opacity-50 disabled:pointer-events-none dark:hs-accordion-active:text-blue-500 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400" aria-expanded="true" aria-controls="hs-basic-collapse-one">
+            <svg class="hs-accordion-active:hidden block size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M5 12h14"></path>
+                <path d="M12 5v14"></path>
+            </svg>
+
+            <svg class="hs-accordion-active:block hidden size-3.5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M5 12h14"></path>
+            </svg>
+            {{ $title }}
+
+        </button>
+        <div id="hs-basic-collapse-one" class="hs-accordion-content w-full overflow-hidden transition-[height] duration-300" role="region" aria-labelledby="hs-basic-heading-one">
+            {{ $content }}
+        </div>
+    </div>
+</div>
--- resources/views/components/list/search-box.blade.php
+++ resources/views/components/list/search-box.blade.php
@@ -1,41 +1,27 @@
-<div id="search-box"
-     class="w-full bg-gray-700"
-     data-accordion="collapse"
-     data-active-classes="bg-gray-400 text-gray-600"
-     data-inacvite-classes="bg-gray-700 text-gray-600"
->
-    <h2 id="accordion-search-box" class="w-full">
-        <button type="button"
-                class="flex items-center justify-between w-full p-2 font-medium rtl:text-right text-gray-500 hover:bg-gray-400 gap-3"
-                data-accordion-target="#accordion-search-box-body"
-                aria-expanded="false"
-                aria-controls="accordion-search-box-body"
-        >
-            <div class="flex justify-center items-center gap-1 text-white">
-                @includeIf('icons.search', ['class' => 'w-[16px] h-[16px]'])
-                検索
-            </div>
-            @includeIf('icons.accordion-arrow', ['class' => 'w-3 h-3 rotate-180 shrink-0 text-white'])
-        </button>
-    </h2>
-    <div id="accordion-search-box-body" class="hidden" aria-labelledby="accordion-search-box">
-        <div class="w-full bg-white border border-b-0 border-gray-200 ">
-            <div class="list-search-box p-3 w-full h-full border bg-white">
-                <h2 class="text-xl font-bold mb-2">{{ __('resource.search_condition') }}</h2>
-                <div class="grid grid-cols-2 gap-2">
-                    {{ $slot }}
-                </div>
-
-                <button
-                    id="btn-search"
-                    class="bg-blue-500 w-full text-white rounded-md mt-4 py-2"
-                >
-                    {{ __('resource.search') }}
-                </button>
-            </div>
-        </div>
+<x-accordian.simple>
+    @slot('title')
+    <div class="flex justify-center items-center gap-1">
+        {{ __('resource.search') }}
+        @includeIf('icons.search', ['class' => 'w-[16px] h-[16px]'])
     </div>
-</div>
+    @endslot
+
+    @slot('content')
+    <div class="list-search-box p-3 w-full h-full border bg-white">
+        <h2 class="text-xl font-bold mb-2">{{ __('resource.search_condition') }}</h2>
+        <div class="grid grid-cols-2 gap-2">
+            {{ $slot }}
+        </div>
+
+        <button
+            id="btn-search"
+            class="bg-blue-500 w-full text-white rounded-md mt-4 py-2"
+        >
+            {{ __('resource.search') }}
+        </button>
+    </div>
+    @endslot
+</x-accordian.simple>
 
 <script type="module">
     $(function() {
