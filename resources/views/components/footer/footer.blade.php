d2ebf3c0465e7ff590916d02c153e142e0bc6367
suupoo <git@suu-poo.com>
2024-08-16 14:22:52 +0900

    🎨:共通になりそうな文言をja.jsonに移設

--- resources/views/components/footer/footer.blade.php
+++ resources/views/components/footer/footer.blade.php
@@ -12,17 +12,17 @@
         <ul class="flex flex-wrap items-center">
             <li class="inline-block relative pe-4 text-xs last:pe-0 last-of-type:before:hidden before:absolute before:top-1/2 before:end-1.5 before:-translate-y-1/2 before:size-[3px] before:rounded-full before:bg-gray-400 dark:text-neutral-500 dark:before:bg-neutral-600">
                 <a class="text-xs text-gray-500 underline hover:text-gray-800 hover:decoration-2 focus:outline-none focus:decoration-2 dark:text-neutral-500 dark:hover:text-neutral-400" target="_blank" href="{{ route('about.terms') }}">
-                    {{ __('menu.terms') }}
+                    {{ __('Terms of service') }}
                 </a>
             </li>
             <li class="inline-block relative pe-4 text-xs last:pe-0 last-of-type:before:hidden before:absolute before:top-1/2 before:end-1.5 before:-translate-y-1/2 before:size-[3px] before:rounded-full before:bg-gray-400 dark:text-neutral-500 dark:before:bg-neutral-600">
                 <a class="text-xs text-gray-500 underline hover:text-gray-800 hover:decoration-2 focus:outline-none focus:decoration-2 dark:text-neutral-500 dark:hover:text-neutral-400" target="_blank" href="{{ route('about.privacy') }}">
-                    {{ __('menu.privacy') }}
+                    {{ __('Privacy-policy') }}
                 </a>
             </li>
             <li class="inline-block relative pe-4 text-xs last:pe-0 last-of-type:before:hidden before:absolute before:top-1/2 before:end-1.5 before:-translate-y-1/2 before:size-[3px] before:rounded-full before:bg-gray-400 dark:text-neutral-500 dark:before:bg-neutral-600">
                 <a class="text-xs text-gray-500 underline hover:text-gray-800 hover:decoration-2 focus:outline-none focus:decoration-2 dark:text-neutral-500 dark:hover:text-neutral-400" target="_blank" href="{{ config('custom.footer.inquiry.form') }}">
-                    {{ __('menu.feedbacks') }}
+                    {{ __('Feedbacks') }}
                 </a>
             </li>
         </ul>
