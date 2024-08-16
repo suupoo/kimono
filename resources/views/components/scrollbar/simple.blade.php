2bf6fee844d16e76deff8bd4407c3fbc2a2fd0ea
suupoo <git@suu-poo.com>
2024-08-16 14:31:58 +0900

    🎨:利用規約用のスクロールバーを追加

---
+++ resources/views/components/scrollbar/simple.blade.php
@@ -0,0 +1,9 @@
+<div class="max-h-[40px] overflow-y-auto border border-transparent
+  [&::-webkit-scrollbar]:w-2
+  [&::-webkit-scrollbar-track]:bg-gray-100
+  [&::-webkit-scrollbar-thumb]:bg-gray-300
+  dark:[&::-webkit-scrollbar-track]:bg-neutral-700
+  dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500
+">
+    {{ $slot }}
+</div>
