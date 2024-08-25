cfe6a3c8ae792a889aebbde95453cb76b9330249
suupoo <git@suu-poo.com>
2024-08-25 16:03:59 +0900

    ♻️:バナー対応に当たりクラス名を変更

--- app/Http/Controllers/SystemController.php
+++ app/Http/Controllers/SystemController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\MSystemFeature as FeatureResourceModel;
-use App\UseCases\SystemAction\Feature\ListAction;
-use App\UseCases\SystemAction\Feature\SaveAction;
+use App\UseCases\SystemAction\Feature\ListAction as FeatureListAction;
+use App\UseCases\SystemAction\Feature\SaveAction as FeatureSaveAction;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class SystemController extends Controller
 {
-    public function listFeature(Request $request, ListAction $action): View
+    public function listFeature(Request $request, FeatureListAction $action): View
     {
         $model = new FeatureResourceModel;
         $items = $action($request, FeatureResourceModel::class);
@@ -19,7 +19,7 @@ class SystemController extends Controller
         return view('system.features.list', compact('model', 'items'));
     }
 
-    public function saveFeature(Request $request, SaveAction $action): RedirectResponse
+    public function saveFeature(Request $request, FeatureSaveAction $action): RedirectResponse
     {
         return $action($request, FeatureResourceModel::class);
     }
