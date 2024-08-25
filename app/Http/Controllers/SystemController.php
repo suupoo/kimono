2c22b1ca2760944ba2772fd128b8456c289bd8b0
suupoo <git@suu-poo.com>
2024-08-25 15:02:27 +0900

    ♻:バナー機能の埋め込みにあたり、モデル指定を変更

--- app/Http/Controllers/SystemController.php
+++ app/Http/Controllers/SystemController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\MSystemFeature as ResourceModel;
+use App\Models\MSystemFeature as FeatureResourceModel;
 use App\UseCases\SystemAction\Feature\ListAction;
 use App\UseCases\SystemAction\Feature\SaveAction;
 use Illuminate\Http\RedirectResponse;
@@ -13,14 +13,14 @@ class SystemController extends Controller
 {
     public function listFeature(Request $request, ListAction $action): View
     {
-        $model = new ResourceModel;
-        $items = $action($request, ResourceModel::class);
+        $model = new FeatureResourceModel;
+        $items = $action($request, FeatureResourceModel::class);
 
         return view('system.features.list', compact('model', 'items'));
     }
 
     public function saveFeature(Request $request, SaveAction $action): RedirectResponse
     {
-        return $action($request, ResourceModel::class);
+        return $action($request, FeatureResourceModel::class);
     }
 }
