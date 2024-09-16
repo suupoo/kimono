99aea4670c414d88edd257de13d3d24dd4bc8071
suupoo <git@suu-poo.com>
2024-09-16 19:36:13 +0900

    ♻️:一覧ページを表示時にデフォルトソートパラメータをカスタマイズ可能に変更

--- app/Http/Controllers/ResourceController.php
+++ app/Http/Controllers/ResourceController.php
@@ -7,20 +7,34 @@ use Illuminate\Http\Request;
 abstract class ResourceController extends Controller
 {
     /**
+     * 一覧表示<index>画面でのデフォルトの一覧表示条件設定
+     * @return array
+     */
+    private function defaultSortParameters(): array
+    {
+        return [
+            'sort'  => 'owner_sequence_no',
+            'order' => 'asc',
+            'rows'  => config('custom.paginate.default'),
+        ];
+    }
+
+    /**
      * 一覧表示前処理
      */
     protected function checkSortParameter(Request $request): array
     {
+        $defaultSortParameters = $this->defaultSortParameters();
         // ソート順番がない場合はリダイレクト
         $redirectParam = [];
         if (! $request->get('sort')) {
-            $redirectParam['sort'] = 'owner_sequence_no';
+            $redirectParam['sort']  = $defaultSortParameters['sort'];
         }
         if (! $request->get('order')) {
-            $redirectParam['order'] = 'asc';
+            $redirectParam['order'] = $defaultSortParameters['order'];
         }
         if (! $request->get('rows')) {
-            $redirectParam['rows'] = config('custom.paginate.default');
+            $redirectParam['rows']  = $defaultSortParameters['rows'];
         }
 
         return $redirectParam;
