36bf54a5e2e61e44ab53cfdc1aaebdad10c1e38d
suupoo <git@suu-poo.com>
2024-09-17 09:23:44 +0900

    🐛:通知ページが表示されない不具合を修正

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -78,84 +78,86 @@
             </div>
         </div>
         <div class="relative overflow-x-auto">
-            @slot('tHead')
-                <tr>
-                    <th scope="col" class="px-6 py-3">
-                        {{ __('resource.operation') }}
-                    </th>
-                    @foreach($model::getColumns() as $column)
-                        @php
-                            if ($column instanceof \App\ValueObjects\Column\Notification\Id) continue;
-                            elseif ($column instanceof \App\ValueObjects\Column\Notification\OwnerSystemCompany) continue;
-                            elseif ($column instanceof \App\ValueObjects\Column\Notification\DeletedAt) continue;
-                        @endphp
-                        <th scope="col" class="px-6 py-3 whitespace-nowrap">
-                            <div class="flex w-full items-center justify-center space-x-1">
-                                @if(in_array($column->column(), $arraySortable))
-                                    <a
-                                        class="p-0.5 @if($sort === $column->column() && $order == 'asc' ) bg-red-400 text-white @else bg-gray-100 text-gray-400 @endif
-                            "
-                                        href="{{ route($currentRouteName, ['sort' => $column->column(), 'order' => 'asc', 'rows' => $rows])}}"
-                                    >
-                                        @include('icons.sort-up')
-                                    </a>
-                                @endif
-
-                                <span>{{ $column->label() }}</span>
-                                @if(in_array($column->column(), $arraySortable))
-                                    <a
-                                        class="p-0.5
-                                @if($sort === $column->column() && $order == 'desc' ) bg-blue-400 text-white @else bg-gray-100 text-gray-400 @endif
-                            "
-                                        href="{{ route($currentRouteName, ['sort' => $column->column(), 'order' => 'desc', 'rows' => $rows])}}"
-                                    >
-                                        @include('icons.sort-down')
-                                    </a>
-                                @endif
-                            </div>
+            <x-table.table>
+                @slot('tHead')
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            {{ __('resource.operation') }}
                         </th>
-                    @endforeach
-                </tr>
-            @endslot
-            @slot('tBody')
-                @foreach($items as $item)
-                    <tr class="bg-white border-b">
-                        <td class="w-full text-xs flex flex-col justify-center space-y-1 m-1">
-                            <x-button.edit type="link"
-                                           href="{{ route($model->getTable() . '.edit', ['id' => $item->id]) }}"/>
-                            <x-button.show type="link"
-                                           href="{{ route($model->getTable() . '.show', ['id' => $item->id]) }}"/>
-                            <x-button.copy type="link"
-                                           href="{{ route($model->getTable() . '.create', ['copy' => $item->id]) }}"/>
-                            <x-button.delete
-                                href="{{ route($model->getTable() . '.destroy', ['id' => $item->id]) }}"
-                                data-id="{{ $item->id }}"
-                            />
-                        </td>
                         @foreach($model::getColumns() as $column)
                             @php
                                 if ($column instanceof \App\ValueObjects\Column\Notification\Id) continue;
                                 elseif ($column instanceof \App\ValueObjects\Column\Notification\OwnerSystemCompany) continue;
                                 elseif ($column instanceof \App\ValueObjects\Column\Notification\DeletedAt) continue;
                             @endphp
-                            <td class="px-6 py-4">
-                                @php
-                                    $columnName = $column->column();
-                                    $value = $item?->$columnName
-                                @endphp
-                                @if($value instanceof UnitEnum )
-                                    {{ $value->label() }}
-                                @else
-                                    {{ $item?->$columnName }}
-                                @endif
-                            </td>
+                            <th scope="col" class="px-6 py-3 whitespace-nowrap">
+                                <div class="flex w-full items-center justify-center space-x-1">
+                                    @if(in_array($column->column(), $arraySortable))
+                                        <a
+                                            class="p-0.5 @if($sort === $column->column() && $order == 'asc' ) bg-red-400 text-white @else bg-gray-100 text-gray-400 @endif
+                                "
+                                            href="{{ route($currentRouteName, ['sort' => $column->column(), 'order' => 'asc', 'rows' => $rows])}}"
+                                        >
+                                            @include('icons.sort-up')
+                                        </a>
+                                    @endif
+
+                                    <span>{{ $column->label() }}</span>
+                                    @if(in_array($column->column(), $arraySortable))
+                                        <a
+                                            class="p-0.5
+                                    @if($sort === $column->column() && $order == 'desc' ) bg-blue-400 text-white @else bg-gray-100 text-gray-400 @endif
+                                "
+                                            href="{{ route($currentRouteName, ['sort' => $column->column(), 'order' => 'desc', 'rows' => $rows])}}"
+                                        >
+                                            @include('icons.sort-down')
+                                        </a>
+                                    @endif
+                                </div>
+                            </th>
                         @endforeach
                     </tr>
-                @endforeach
-            @endslot
-            @slot('pagination')
-                {{ $items->links() }}
-            @endslot
+                @endslot
+                @slot('tBody')
+                    @foreach($items as $item)
+                        <tr class="bg-white border-b">
+                            <td class="w-full text-xs flex flex-col justify-center space-y-1 m-1">
+                                <x-button.edit type="link"
+                                               href="{{ route($model->getTable() . '.edit', ['id' => $item->id]) }}"/>
+                                <x-button.show type="link"
+                                               href="{{ route($model->getTable() . '.show', ['id' => $item->id]) }}"/>
+                                <x-button.copy type="link"
+                                               href="{{ route($model->getTable() . '.create', ['copy' => $item->id]) }}"/>
+                                <x-button.delete
+                                    href="{{ route($model->getTable() . '.destroy', ['id' => $item->id]) }}"
+                                    data-id="{{ $item->id }}"
+                                />
+                            </td>
+                            @foreach($model::getColumns() as $column)
+                                @php
+                                    if ($column instanceof \App\ValueObjects\Column\Notification\Id) continue;
+                                    elseif ($column instanceof \App\ValueObjects\Column\Notification\OwnerSystemCompany) continue;
+                                    elseif ($column instanceof \App\ValueObjects\Column\Notification\DeletedAt) continue;
+                                @endphp
+                                <td class="px-6 py-4">
+                                    @php
+                                        $columnName = $column->column();
+                                        $value = $item?->$columnName
+                                    @endphp
+                                    @if($value instanceof UnitEnum )
+                                        {{ $value->label() }}
+                                    @else
+                                        {{ $item?->$columnName }}
+                                    @endif
+                                </td>
+                            @endforeach
+                        </tr>
+                    @endforeach
+                @endslot
+                @slot('pagination')
+                    {{ $items->links() }}
+                @endslot
+            </x-table.table>
         </div>
     </div>
 @endsection
