1ca2f0490de9b58fc0fb8d1c0958b350e21b8e8e
suupoo <git@suu-poo.com>
2024-10-31 23:12:57 +0900

    🎨:ダッシュボードでモバイル/それ以上でレイアウト調整

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,8 +5,8 @@
       'now' => \Carbon\CarbonImmutable::now(),
     ])
     <div class="flex w-full flex flex-col">
-        <div class="flex flex-row w-full gap-1">
-            <div class="custom-dashboard-item w-1/3 flex flex-col justify-center items-center">
+        <div class="flex flex-col md:flex-row w-full gap-1">
+            <div class="custom-dashboard-item w-full md:w-1/3 flex flex-col justify-center items-center">
                 <h2 class="text-3xl font-bold text-center">
                     <div class="flex flex-col gap-1 justify-center items-center">
                         <div>
@@ -18,7 +18,7 @@
                     </div>
                 </h2>
             </div>
-            <div class="custom-dashboard-item text-sm gap-1 w-2/3 flex flex-col items-end">
+            <div class="custom-dashboard-item text-sm gap-1 w-full md:w-2/3 flex flex-col items-end">
                 <div class="font-bold text-lg">
                     {{ __("Today's New Create Records") }}：{{ $newRecords->sum('count'). __('Records') }}
                 </div>
